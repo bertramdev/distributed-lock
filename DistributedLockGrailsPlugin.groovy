e219bd1cd064f38d9108266286e92cd346276599
jsaardchit <jordon@saardchit.com>
2014-06-17 00:09:34 -0700

    Changing plugin version to snapshot

--- DistributedLockGrailsPlugin.groovy
+++ DistributedLockGrailsPlugin.groovy
@@ -2,7 +2,7 @@ import com.bertram.lock.conf.LockServiceConfigurer
 
 class DistributedLockGrailsPlugin {
     // the plugin version
-    def version = "0.2"
+    def version = "0.2-SNAPSHOT"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.2 > *"
     // resources that are excluded from plugin packaging
